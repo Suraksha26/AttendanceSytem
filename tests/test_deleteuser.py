b93ec3795e360b39ecf010b06cbe56370353942e
Suraksha26 <63924341+Suraksha26@users.noreply.github.com>
2024-02-06 23:35:56 +0530

    Delete tests/test_deleteuser.py

--- tests/test_deleteuser.py
+++
@@ -1,4 +0,0 @@
-def test_delete_user(client):
-    response = client.get('/deleteuser?user=testuser')
-    assert response.status_code == 200
-    assert b'User deleted successfully' in response.data
